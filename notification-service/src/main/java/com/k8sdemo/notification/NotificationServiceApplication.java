31d46e7840d56e3931afe183e272e3f4095ea97c
Ben Hampton <benhampton11@yahoo.com>
2026-07-11 10:18:25 -0500

    init commit of notification-service

---
+++ notification-service/src/main/java/com/k8sdemo/notification/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.k8sdemo.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
